d3421307156fc47af6b99de4ffce419ca5b95ae8
Geraldine R <hello@geraldiner.com>
2025-12-06 14:30:06 -0500

    feat: Project setup with Phaser and TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    ignores: ["node_modules", "dist"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: process.cwd(),
+      },
+    },
+  },
+);
