fe126ed318548a4eab5f7635ec9cac54de3ecde5
Geraldine R <hello@geraldiner.com>
2025-12-10 20:37:50 -0500

    feat: Update Title Scene, add simple Prelude Scene to show before Game Scene

--- src/scenes/title-scene.ts
+++ src/scenes/title-scene.ts
@@ -8,6 +8,34 @@ export class TitleScene extends Phaser.Scene {
   }
 
   public create(): void {
-    this.scene.start(SCENE_KEYS.GAME);
+    // DEBUG: Skip title scene
+    // this.scene.start(SCENE_KEYS.GAME);
+
+    this.add
+      .text(this.scale.width / 2, this.scale.height / 2 - 25, "Don't Be Late!")
+      .setOrigin(0.5)
+      .setColor("#000000");
+
+    const clickToStartText = this.add
+      .text(this.scale.width / 2, this.scale.height / 2 + 25, "Click to Start")
+      .setOrigin(0.5)
+      .setColor("#000000");
+
+    this.tweens.add({
+      targets: clickToStartText,
+      alpha: { from: 1, to: 0 },
+      duration: 700,
+      yoyo: true,
+      repeat: -1,
+    });
+
+    this.input.once(Phaser.Input.Events.POINTER_DOWN, () => {
+      this.cameras.main.fadeOut(500, 0, 0, 0, (_, progress: number) => {
+        if (progress !== 1) {
+          return;
+        }
+        this.scene.start(SCENE_KEYS.GAME);
+      });
+    });
   }
 }
